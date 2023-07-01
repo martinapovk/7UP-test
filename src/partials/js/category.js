9c0222d0b28f4384235a30edd152ea9c49bae27f
Olena-Rudnieva <rudnievao@gmail.com>
2023-07-01 23:15:04 +0300

    Update styles

---
+++ src/partials/js/category.js
@@ -0,0 +1,28 @@
+import BooksApiService from './BooksApiService';
+// import onCategoryHandle from './onCategoryHandle';
+
+const allCategoriesRef = document.querySelector('.all-categories-list');
+const titleRef = document.querySelector('.title');
+
+const api = new BooksApiService();
+
+export function renderCategoryName(option) {
+  const words = option.list_name.split(' ');
+  const accentWord = words.pop();
+  const blackWords = words.join(' ');
+  const markup = `${blackWords} <span class="title--accent">${accentWord}</span>`;
+  return markup;
+}
+
+export function renderBook(option) {
+  const markup = option
+    .map(
+      option => `<li class="selected-category-item">
+                     <img class="best-sellers-book__img" src="${option.book_image}" alt="Book">
+                     <p class="-best-sellers-book__title">${option.title}</p>
+                     <p class="best-sellers-book__author">${option.author}</p>
+                  </li>`
+    )
+    .join('');
+  return markup;
+}
