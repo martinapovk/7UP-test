5f2fe5597220003f95f9126b62e105ee9137ee64
martinapovk <martinacskapok@gmail.com>
2023-07-02 03:23:15 +0300

    Update category.js

--- src/partials/js/category.js
+++ src/partials/js/category.js
@@ -1,10 +1,10 @@
-import BooksApiService from './BooksApiService';
+// import BooksApiService from './BooksApiService';
 // import onCategoryHandle from './onCategoryHandle';
 
-const allCategoriesRef = document.querySelector('.all-categories-list');
-const titleRef = document.querySelector('.title');
+// const allCategoriesRef = document.querySelector('.all-categories-list');
+// const titleRef = document.querySelector('.title');
 
-const api = new BooksApiService();
+// const api = new BooksApiService();
 
 export function renderCategoryName(option) {
   const words = option.list_name.split(' ');
@@ -17,7 +17,7 @@ export function renderCategoryName(option) {
 export function renderBook(option) {
   const markup = option
     .map(
-      option => `<li class="selected-category-item">
+      option => `<li class="selected-category-item" data-book-id="${option._id}">
                      <img class="best-sellers-book__img" src="${option.book_image}" alt="Book">
                      <p class="-best-sellers-book__title">${option.title}</p>
                      <p class="best-sellers-book__author">${option.author}</p>
