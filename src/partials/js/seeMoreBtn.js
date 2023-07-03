b1d1c0ad875e4d04ce37bb51ac3247d4d1ff4d1c
martinapovk <martinacskapok@gmail.com>
2023-07-04 00:42:14 +0300

    Update seeMoreBtn.js

--- src/partials/js/seeMoreBtn.js
+++ src/partials/js/seeMoreBtn.js
@@ -20,6 +20,16 @@ function onBtnSeeMoreHandle(e) {
   //   console.log(e.target.dataset.btnCategoryName);
 
   removeAccentFromBestSellersTitle();
+  const allCategoriesList = document.querySelector('.all-categories-list');
+  const allCategories = [...allCategoriesList.children];
+  // console.log(e.target.dataset.btnCategoryName);
+  allCategories
+    .find(
+      category =>
+        category.dataset.categoryName === e.target.dataset.btnCategoryName
+    )
+    .classList.add('current-category');
+  // console.log(allCategories[0].dataset.categoryName);
   createCategory(e.target.dataset.btnCategoryName);
 }
 
